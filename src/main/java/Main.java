757114a15843334a552b6351c1714e9200fe8d3d
Daniel Ramos Fagundes <32979813-sandra489632052@users.noreply.replit.com>
2024-03-22 00:04:38 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,36 @@
+// Programa Imprime uma Mensagem na tela
+// Autor: Daniel Ramos Fagundes
+
+public class Main {
+  public static void main(String[] args) {
+    // System.out.println("Hello world!");
+
+    
+//Variaveis do tipo booleano
+boolean media = true;
+boolean frequencia = false;
+boolean aprovado = media && frequencia;
+boolean cafe = false;
+boolean agua = true;
+boolean clienteServido = cafe || agua;
+
+// Variaveis do tipo inteiro
+int idade = 20;
+
+// Variavel do tipo real
+double salario = 1000.00;
+float temperatura = 25.8f;
+
+// Variavel do tipo texto
+String nome = "Daniel";
+String cpf = "12345678910";
+String aniversario = "01/01/2000";
+
+
+
+
+// Método de saída
+// System.out.println("Cliente Servido? " + clienteServido);
+    
+  }
+}
